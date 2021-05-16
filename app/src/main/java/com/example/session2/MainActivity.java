a20f32ebe92a2b8aa097b5a934ab15a4700f4a01
dhawal <dhawalsharma10.ds@gmail.com>
2021-05-17 00:13:01 +0530

    Add intent from MainActivity to SecondActivity

--- app/src/main/java/com/example/session2/MainActivity.java
+++ app/src/main/java/com/example/session2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.session2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,11 +10,15 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.jar.Attributes;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG ="MainActivity" ;
     private TextView mTextView;
     private Button mButton;
+
+    public static final String NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +28,16 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = findViewById(R.id.textView);
         mButton = findViewById(R.id.button);
+        mTextView.setText("dhawal");
 
         mButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Log.e(TAG, "onClick: " + mTextView.getText().toString());
-               Toast.makeText(MainActivity.this,"Button Pressed!",Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent( MainActivity.this, SecondActivity.class);
+                intent.putExtra(NAME , mTextView.getText().toString());
+                startActivity(intent);
 
             }
         });
